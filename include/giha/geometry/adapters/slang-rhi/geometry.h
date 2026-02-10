45ae74e0c806716a5104e9f800c59fc2f03aeb42
hyeonjang <hyeonjang2021@gmail.com>
2026-02-10 23:07:13 +0900

    add atomic flip

--- include/giha/geometry/adapters/slang-rhi/geometry.h
+++ include/giha/geometry/adapters/slang-rhi/geometry.h
@@ -18,14 +18,16 @@ struct SigmaModel {
 
     template <typename Id, typename Key>
     SigmaModel(rhi::IDevice* device, const giha::DartMap<Id, Key>& map, uint32_t padd = 0) {
-        cpuCounter = static_cast<uint32_t>(map.count());
         idSize = sizeof(Id);
         keySize = sizeof(Key);
 
-        counter = createBuffer(device, &cpuCounter, 1);
         vertNext = createBuffer(device, map.vNext.data(), map.vNext.size() + padd);
         edgeNext = createBuffer(device, map.eNext.data(), map.eNext.size() + padd);
         inducedVertexMap = createBuffer(device, map.vKeys.data(), map.vKeys.size() + padd);
+
+        cpuCounter = static_cast<uint32_t>(map.count());
+        counter = createBuffer(device, &cpuCounter, 1);
+        ownershipFlags = createBuffer(device, nullptr, sizeof(i32) * (map.count() + padd));
     }
 
     void writeInto(rhi::ShaderCursor cursor) const {
@@ -44,14 +46,10 @@ struct SigmaModel {
             CHECK_SLANG(keyCursor.setBinding(inducedVertexMap), "Failed to bind inducedVertexMap");
         }
 
-        // for mutable
-        auto counterCursor = cursor.getPath("counter");
-        if (counterCursor.isValid()) {
-            CHECK_SLANG(counterCursor.setBinding(counter), "Failed to bind counter");
-        }        
-        
+        // for mutable        
         auto modelCursor = cursor.getPath("model");
         if (modelCursor.isValid()) {
+
             auto vertCursor = modelCursor.getPath("vertNext");
             CHECK(vertCursor.isValid(), "Missing SigmaModel.vertNext");
             CHECK_SLANG(vertCursor.setBinding(vertNext), "Failed to bind vertNext");
@@ -64,22 +62,34 @@ struct SigmaModel {
             CHECK(keyCursor.isValid(), "Missing SigmaModel.inducedVertexMap");
             CHECK_SLANG(keyCursor.setBinding(inducedVertexMap), "Failed to bind inducedVertexMap");
 
+            auto counterCursor = cursor.getPath("counter");
+            if (counterCursor.isValid()) {
+                CHECK_SLANG(counterCursor.setBinding(counter), "Failed to bind counter");
+            }
+
             auto vertNextAtomicCursor = cursor.getPath("vertNextAtomic");
             auto edgeNextAtomicCursor = cursor.getPath("edgeNextAtomic");
             if (vertNextAtomicCursor.isValid() && edgeNextAtomicCursor.isValid()) {
                 CHECK_SLANG(vertNextAtomicCursor.setBinding(vertNext), "Failed to bind vertNextAtomic");
                 CHECK_SLANG(edgeNextAtomicCursor.setBinding(edgeNext), "Failed to bind edgeNextAtomic");
             }
+
+            auto ownershipFlagsCursor = cursor.getPath("ownershipFlags");
+            if (ownershipFlagsCursor.isValid()) {
+                CHECK_SLANG(ownershipFlagsCursor.setBinding(ownershipFlags), "Failed to bind ownershipFlags");
+            }
         }
     }
 
     size_t idSize, keySize;
 
-    uint32_t cpuCounter;
-    Slang::ComPtr<rhi::IBuffer> counter;
     Slang::ComPtr<rhi::IBuffer> vertNext;
     Slang::ComPtr<rhi::IBuffer> edgeNext;
     Slang::ComPtr<rhi::IBuffer> inducedVertexMap;
+
+    uint32_t cpuCounter;
+    Slang::ComPtr<rhi::IBuffer> counter;
+    Slang::ComPtr<rhi::IBuffer> ownershipFlags; // int32
 };
 
 // 
